b2083db48f59431425a09ad1a11cfe77977001c9
= <cre8.hystory@gmail.com>
2025-05-02 18:35:43 +0530

    pushed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SocialIcons from "./compononets/SocialIcon";
 import Form from "./compononets/Form";
@@ -7,36 +6,6 @@ import "/src/styles/form.css";
 import "/src/styles/app.css";
 
 function App() {
-  const [ConditionWd, setConditionWd] = useState(window.innerWidth > 800);
-  const mainElementRef = useRef(null);
-  useEffect(() => {
-    const handelResize = () => {
-      setConditionWd(window.innerWidth > 800);
-    };
-
-    window.addEventListener("resize", handelResize);
-
-    // if (mainElementRef.current) {
-    //   if (ConditionWd) {
-    //     if (!mainElementRef.current.classList.contains("m-5")) {
-    //       mainElementRef.current.classList.add("m-5");
-    //     }
-    //   } else {
-    //     if (mainElementRef.current.classList.contains("m-5")) {
-    //       mainElementRef.current.classList.remove("m-5");
-    //     }
-    //   }
-    // }
-  }, []);
-
-  useEffect(() => {
-    if (mainElementRef.current) {
-      ["m-5"].forEach((cls) => {
-        mainElementRef.current.classList.toggle(cls, ConditionWd);
-      });
-    }
-  }, [ConditionWd]);
-
   return (
     <>
       <div className="container row p-4 m-4 d-flex justify-content-between align-items-center flex-row-reverse">
